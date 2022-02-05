445c3c3b4ee5df36f940219c6500f22ac46966af
zubairwazir <zubairwazir777@gamil.com>
2022-02-06 01:51:44 +0500

    adding intro video to my site

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Entrepreneur", "Designer", "Freelancer", "Blogger"],
+        strings: ["Python", "Flask", "FastAPI", "CI and CD", "ML and DL", "SQL and NoSQL", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Entrepreneur", "Designer", "Freelancer", "Blogger"],
+        strings: ["Python", "Flask", "FastAPI", "CI and CD", "ML and DL", "SQL and NoSQL", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
